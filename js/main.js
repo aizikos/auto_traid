5b5ad2dd8f779a515099ba757759542826e90f76
Aizirek Abdijalilova <aaizirek499@gmail.com>
2023-05-05 19:37:39 +0300

    brand file

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ document.addEventListener("DOMContentLoaded",function(){
         document.querySelector(".header-content").classList.toggle(".open")
     })
 });
+//для ссылки!
+
+var rem = function(evt){
+    evt.preventDefault();
+    let link = document.getElementsByClassName(".remove")
+   
+}
