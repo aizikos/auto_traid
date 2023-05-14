5d0039298218f850febebeeaf15cd740acaac66e
Aizirek Abdijalilova <aaizirek499@gmail.com>
2023-05-15 02:03:17 +0300

    started the third page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+///Для бургер меню
+
 document.addEventListener("DOMContentLoaded",function(){
     document.getElementById("burger").addEventListener("click",function(){
         document.querySelector(".header-content").classList.toggle("open")
     })
 });
 
-
+/// Для скытя текста
 function readMore(){
     var dots = document.getElementById("dots");
     var more = document.getElementById("more");
