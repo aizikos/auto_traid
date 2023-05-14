6b15de033093c6ad918cc7aab0d7adcc95849f8b
Aizirek Abdijalilova <aaizirek499@gmail.com>
2023-05-14 18:27:26 +0300

    Media query for the second page

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@ document.addEventListener("DOMContentLoaded",function(){
         document.querySelector(".header-content").classList.toggle("open")
     })
 });
+
+
+function readMore(){
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btn = document.getElementById("btn");
+
+    if(dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "More";
+        more.style.display = "none";
+        
+    }else{
+            dots.style.display = "none";
+            btn.innerHTML = "keep back";
+            more.style.display = "inline";
+        }
+    
+}
