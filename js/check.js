6d0ff0f509c873cbf1b962a7df9cffcbe16bd6ee
Aizirek Abdijalilova <aaizirek499@gmail.com>
2023-05-25 23:16:37 +0300

    finished block "Body Type"

---
+++ js/check.js
@@ -0,0 +1,10 @@
+// var chbox;
+// var lab ; 
+// lab= document.getElementById('lab')
+//     chbox=document.getElementById('one');
+//         if (chbox.checked) {
+//             lab.style.backgroundColor='green';
+//         }
+//         else {
+//             lab.style.backgroundColor = 'blue'
+//         }
