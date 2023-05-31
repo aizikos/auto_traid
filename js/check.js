2a654c66d2599544d04e1c455db849796631d2e0
Aizirek Abdijalilova <aaizirek499@gmail.com>
2023-05-31 22:57:38 +0300

    added projeckt

--- js/check.js
+++ js/check.js
@@ -1,10 +1,26 @@
-// var chbox;
-// var lab ; 
-// lab= document.getElementById('lab')
-//     chbox=document.getElementById('one');
-//         if (chbox.checked) {
-//             lab.style.backgroundColor='green';
-//         }
-//         else {
-//             lab.style.backgroundColor = 'blue'
-//         }
+///Для бургер меню
+
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("burger").addEventListener("click",function(){
+        document.querySelector(".header-content").classList.toggle("open")
+    })
+});
+
+/// Для скытя текста
+function readMore(){
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btn = document.getElementById("btn");
+
+    if(dots.style.display === "none") {
+        dots.style.display = "flex";
+        btn.innerHTML = "More";
+        more.style.display = "none";
+        
+    }else{
+            dots.style.display = "none";
+            btn.innerHTML = "Skip";
+            more.style.display = "flex";
+        }
+    
+}
